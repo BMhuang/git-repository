893984a2d404c7e691820c2bf3cb9f3ffcf37417
BMhuang <48756384+BMhuang@users.noreply.github.com>
2021-07-30 09:59:41 +0800

    在githello中增加一句话

--- src/main/java/com/bigboss/gittwice/githello.java
+++ src/main/java/com/bigboss/gittwice/githello.java
@@ -3,5 +3,6 @@ package com.bigboss.gittwice;
 public class githello {
     public static void main(String[] args) {
         System.out.println("githello");
+        System.out.println("githello1");
     }
 }
